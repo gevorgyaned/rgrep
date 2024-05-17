22008a43547ac08dbd8fecdac59914d0aaf6346c
gevorgyaned <eduardgev00@gmail.com>
2024-05-17 19:48:30 +0400

    added directory support

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,22 @@
 use clap::Parser;
 use colored::Colorize;
-use std::{fmt, fs::File, io};
+use std::{ffi::OsString, fmt, io, path::PathBuf};
 
-use crate::wildcard_match::search_in_file;
+use crate::wildcard_match::log_occurances;
 
 mod wildcard_match;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
-    pattern: String,
+    wildcard: String,
     filename: Vec<String>,
 }
 
 impl fmt::Display for Args {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{} {:?}", self.pattern, self.filename)
+        write!(f, "{} {:?}", self.wildcard, self.filename)
     }
-}
-
-fn log_occurances(file_name: &str, pattern: &str) -> Result<(), &'static str> {
-    let file = match File::open(file_name) {
-        Ok(f) => f,
-        Err(_) => return Err("unable to open file"),
-    };
-
-    let matches = search_in_file(file, pattern);
-
-    if matches.is_empty() {
-        return Err("no occurances is found");
-    }
-
-    println!("{}: ", file_name.green());
-    matches
-        .iter()
-        .for_each(|match_entry| println!("{}", match_entry));
-
-    Ok(())
 }
 
 fn main() -> io::Result<()> {
@@ -45,9 +25,9 @@ fn main() -> io::Result<()> {
     let file_names = args.filename;
 
     for file_name in file_names {
-        match log_occurances(file_name.as_str(), &args.pattern) {
+        match log_occurances(&PathBuf::from(OsString::from(&file_name)), &args.wildcard) {
             Ok(_) => (),
-            Err(err) => eprintln!("{}: \n{}", file_name.red(), err),
+            Err(err) => println!("{}: \n{}", file_name.red(), err),
         }
     }
 
--- src/wildcard_match.rs
+++ src/wildcard_match.rs
@@ -1,30 +1,89 @@
 use colored::*;
 use core::fmt;
 use std::{
-    fs::File,
-    io::{self, BufRead},
+    fs::{self, File},
+    io::{self, BufRead}, path::PathBuf,
 };
 
 pub struct MatchedLine {
-    pub line: String,
+    pub line: Vec<String>,
     pub line_number: usize,
+    pub word_number: usize,
 }
 
 impl MatchedLine {
-    pub fn new(line: String, line_number: usize) -> MatchedLine {
-        MatchedLine { line, line_number }
+    pub fn new(line: Vec<String>, line_number: usize, word_number: usize) -> MatchedLine {
+        MatchedLine { line, line_number, word_number }
     }
 }
 
 impl fmt::Display for MatchedLine {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
+        let _ = write!(
             f,
-            "{}: {}",
+            "{}:",
             self.line_number.to_string().blue(),
-            self.line.cyan()
-        )
+        );
+
+        for (idx, word) in self.line.iter().enumerate() {
+            if idx == self.word_number {
+                let _ = write!(f, "{}", word.red());
+            } else {
+                let _ = write!(f, "{}", word);
+            }
+        }
+
+        Ok(())
     }
+}
+
+pub fn log_occurances(file_name: &PathBuf, wildcard: &str) -> Result<(), &'static str> {
+    if file_name.is_dir() {
+        println!("Directory {}: ", file_name.display().to_string().green());
+
+        let file_names = match fs::read_dir(file_name) {
+            Ok(f) => f,
+            Err(_) => return Err("unable to read directory"),
+        };
+
+        for res_file_name in file_names {
+            let file_name = match res_file_name {
+                Ok(e) => e,
+                Err(_) => return Err("unable to resolve"),
+            };
+
+            let _ = log_occurances(&file_name.path(), wildcard);
+        }
+
+        return Ok(());
+    }
+
+    let file = match File::open(file_name) {
+        Ok(f) => f,
+        Err(_) => return Err("unable to open file"),
+    };
+
+    let matches = search_in_file(file, wildcard);
+    if matches.is_empty() {
+        return Err("no occurances is found");
+    }
+
+    println!("{}: ", file_name.display().to_string().green());
+    matches
+        .iter()
+        .for_each(|match_entry| println!("{}", match_entry));
+
+    Ok(())
+}
+
+fn parse(input: &str) -> Option<Vec<String>> {
+    if input.is_empty() {
+        return None;
+    }
+
+    Some(input.split_ascii_whitespace()
+        .map(|s| s.to_string())
+        .collect())
 }
 
 pub fn is_match(str: &str, pattern: &str) -> bool {
@@ -65,12 +124,21 @@ pub fn search_in_file(file: File, pattern: &str) -> Vec<MatchedLine> {
     let mut result = Vec::new();
     let lines = io::BufReader::new(file).lines();
 
-    for (idx, string) in lines.enumerate() {
+    for (line_idx, string) in lines.enumerate() {
         if let Ok(line) = string {
-            let words = line.split(' ');
+            let words = match parse(&line) {
+                Some(w) => w,
+                None => continue,
+            };
 
-            if words.into_iter().any(|word| is_match(word, pattern)) {
-                result.push(MatchedLine::new(line, idx + 1));
+            for (word_idx, word) in words.iter().enumerate() {
+                if is_match(word.trim(), pattern) {
+                    result.push(MatchedLine::new(
+                        words.clone(), 
+                        line_idx, 
+                        word_idx));
+                    break;
+                }
             }
         }
     }
